2c45808463a5f9b3a312a9adcb1f7c0263d5bf7f
Samiksusamik <sam.kipnis@gmail.com>
2024-02-02 09:53:37 -0500

    e

---
+++ reactive-app/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+	import Form from './components/Form.vue';
+	import './style.css';
+</script>
+
+<template>
+	<div>
+		<Form />
+	</div>
+</template>
---
+++ reactive-app/src/components/Form.vue
@@ -0,0 +1,4 @@
+<script setup></script>
+<template>
+	<h1 class="text-4xl">Hi</h1>
+</template>
