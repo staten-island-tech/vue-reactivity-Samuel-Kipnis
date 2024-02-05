4be5799205d3c03c671b2a7a30488067565fb6ae
Samiksusamik <sam.kipnis@gmail.com>
2024-02-05 10:15:09 -0500

    e

--- thing/src/views/FormView.vue
+++ thing/src/views/FormView.vue
@@ -1,7 +1,42 @@
 <template>
-	<div></div>
+	<form @submit.prevent="addPost">
+		<div>
+			<label for="title">Title</label
+			><input type="text" id="title" :value="title" />
+		</div>
+		<div>
+			<label for="body">Body</label
+			><input type="text" id="body" :value="body" />
+		</div>
+		<div>
+			<label for="author">Author Name</label
+			><input type="text" id="author" :author="author" />
+		</div>
+		<button type="submit">Submit</button>
+	</form>
 </template>
 
-<script></script>
+<script setup>
+	import { ref } from 'vue';
+	import uuid from 'uuidv4';
+
+	import { usePostsStore } from '../stores/posts';
+
+	const title = ref('');
+	const body = ref('');
+	const author = ref('');
+
+	function addPost() {
+		const store = usePostsStore();
+		const id = uuid();
+
+		store.addPost({
+			title,
+			author,
+			body,
+			id,
+		});
+	}
+</script>
 
 <style scoped></style>
